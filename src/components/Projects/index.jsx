08a02bd8c576abad1fe9c276a55e83512dadcaa7
Kelvin Chao <kelvinchao2011@gmail.com>
2022-10-04 16:41:43 -0700

    created cards and project data

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,21 +1,43 @@
-import { Box, Typography, Button } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Button,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Grid,
+} from "@mui/material";
 
-import { SidebarData } from "../SideDrawer/SidebarData";
+import { ProjectData } from "./projectData";
 import { ComponentBox } from "../reusables";
 
 function Projects() {
   return (
     <ComponentBox>
       <Typography variant="h1">Projects</Typography>
-      <Box>
-        <h1>Resume</h1>
-        {Object.entries(SidebarData).map(([key, item]) => (
-          <p key={key}>
-            {item.title}
-            {key}
-          </p>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {Object.entries(ProjectData).map(([index, item]) => (
+          <Grid item xs={2} sm={4} md={4} key={index}>
+          <Card sx={{ maxWidth: 400 }}>
+            <CardMedia />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {item.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Github</Button>
+              <Button size="small">Deployed</Button>
+            </CardActions>
+          </Card>
+          </Grid>
         ))}
-      </Box>
+      </Grid>
     </ComponentBox>
   );
 }
---
+++ src/components/Projects/projectData.js
@@ -0,0 +1,44 @@
+export const ProjectData = [
+  {
+    title: 'a',
+    image: '',
+    description: '',
+    githubLink: '',
+    deployLink: ''
+  },
+  {
+    title: 'b',
+    image: '',
+    description: '',
+    githubLink: '',
+    deployLink: ''
+  },
+  {
+    title: 'c',
+    image: '',
+    description: '',
+    githubLink: '',
+    deployLink: ''
+  },
+  {
+    title: 'd',
+    image: '',
+    description: '',
+    githubLink: '',
+    deployLink: ''
+  },
+  {
+    title: 'e',
+    image: '',
+    description: '',
+    githubLink: '',
+    deployLink: ''
+  },
+  {
+    title: 'f',
+    image: '',
+    description: '',
+    githubLink: '',
+    deployLink: ''
+  },
+]
