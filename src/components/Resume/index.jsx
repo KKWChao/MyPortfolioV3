5ebfe49f0f71a2933a9a44062c19eac7995c3572
Kelvin Chao <kelvinchao2011@gmail.com>
2022-10-05 00:48:55 -0700

    building resume component

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -1,14 +1,46 @@
 import { Box, Typography, Button } from "@mui/material";
 import { ComponentBox } from "../reusables";
-import { SidebarData } from "../SideDrawer/SidebarData";
+import { theme } from "../../theme";
+
+import { ResumeData } from "./resumeData";
 
 function Resume() {
   return (
     <ComponentBox>
-      <h1>Resume</h1>
-      {Object.entries(SidebarData).map(([key, item]) => (
-      <p key={key}>{item.title}{key}</p>
-      ))}
+      <Typography variant="h1">Resume</Typography>
+      <Box
+        sx={{
+          backgroundColor: "white",
+          height: "70%",
+        }}
+      >
+        <Box className="header">
+          <Typography>{ResumeData[0].summary}</Typography>
+        </Box>
+        <Box className="skills">
+          <Typography variant="h3">{ResumeData[1].title}</Typography>
+          <Typography>
+            Front End: <strong>{ResumeData[1].frontEnd}</strong>
+            <br />
+            Back End: <strong>{ResumeData[1].backEnd}</strong>
+            <br />
+            Concepts: <strong>{ResumeData[1].concepts}</strong>
+            <br />
+            Tools: <strong>{ResumeData[1].tools}</strong>
+            <br />
+          </Typography>
+        </Box>
+        <Box className="projects">
+          <Typography variant="h3">{ResumeData[2].title}</Typography>
+        </Box>
+        <Box className="experience">
+          <Typography variant="h3">{ResumeData[3].title}</Typography>
+        </Box>
+        <Box className="education">
+          <Typography variant="h3">{ResumeData[4].title}</Typography>
+        </Box>
+        <Button sx={{ color: theme.palette.text.dark }}>Download</Button>
+      </Box>
     </ComponentBox>
   );
 }
